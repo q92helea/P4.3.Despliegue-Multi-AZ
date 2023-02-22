9802154f1cec96864b227df7cd03a9e9e468740d
Andres <q92helea@hotmail.com>
2023-02-22 11:47:14 +0100

    prueba

---
+++ docs/download/archivosAmelia/formularioAmelia/grabar.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+ <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <header>
+      <form id="form">
+        <input type="text" id="search" class="search" placeholder="Search">
+      </form>
+    </header>
+<main id="main">
+<?php
+include ('conexion.php');
+$nombre=$_REQUEST['nombre'];
+$donativo=$_REQUEST['donativo'];
+$tipomoneda=$_REQUEST['tipomoneda'];
+$data = [
+    'nombre' => $nombre,
+    'donativo' => $donativo,
+    'tipomoneda' => $tipomoneda,
+];
+$sql = "INSERT INTO donativos VALUES (null,:nombre, :donativo, :tipomoneda)";
+$stmt= $conn->prepare($sql);
+$stmt->execute($data);
+echo "<div class='center'>";
+echo "<h2> Donacion registrada correctamente. Gracias por su colaboracion </h2>";
+echo "<h2><a href='index.html' > Volver a Inicio </a></h2>";
+echo "</div>";
+echo "<div class='center' style='font-size:25px;color: #FF0000'>";
+ $sql = 'SELECT sum(donativo) as total from donativos;';
+    foreach ($conn->query($sql) as $row) {
+        echo "TOTAL RECAUDADO ".$row['total'] . "\n";
+    }
+echo "</div>";
+?>
+</main>
+</body>
+</html>
---
+++ docs/download/docs/formulario.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+ <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<header>
+      <form id="form">
+        <input type="text" id="search" class="search" placeholder="Search">
+      </form>
+</header>
+<main id="main">
+<form action="grabar.php" method="post" class="form">
+     <label ><h1> DONATIVOS PARA TURQUIA Y SIRIA </h1></label>
+    <label for="name">Andres Heras :</label>
+    <input type="text" id="nombre" name="nombre">
+    <br>
+<label for="donativo">Donativo: </label>
+    <input type="donativo" id="donativo" name="donativo">
+    <br>
+<input type="radio" name="tipomoneda" value="euro" checked > Euros <br>
+
+    <input type="radio" name="tipomoneda" value="dolar"> Dolares <br>
+    <br>
+<input type="submit" value ="Enviar" name="enviar" />
+
+</form>
+
+</body>
+</html>
